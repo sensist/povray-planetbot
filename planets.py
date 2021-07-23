f473df37896e1fdee1e74ab5096822ab8b62d312
Sean Sisti <sean.sisti@gmail.com>
2021-07-23 14:15:52 -0400

    re-added tweepy auth

--- planets.py
+++ planets.py
@@ -4,27 +4,21 @@ from os import environ
 
 import tweepy
 
-'''
-CONSUMER_KEY = environ['CONSUMER_KEY']
-CONSUMER_SECRET = environ['CONSUMER_SECRET']
-ACCESS_KEY = environ['ACCESS_KEY']
-ACCESS_SECRET = environ['ACCESS_SECRET']
+def tweepy_creds():
+    CONSUMER_KEY = environ['CONSUMER_KEY']
+    CONSUMER_SECRET = environ['CONSUMER_SECRET']
+    ACCESS_KEY = environ['ACCESS_KEY']
+    ACCESS_SECRET = environ['ACCESS_SECRET']
 
-print(CONSUMER_KEY)
-print(CONSUMER_SECRET)
-print(ACCESS_KEY)
-print(ACCESS_SECRET)
+    # Authenticate to Twitter
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 
-# Authenticate to Twitter
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+    # Create API object
+    return tweepy.API(auth)
 
-# Create API object
-api = tweepy.API(auth)
 
-# Create a tweet
-#api.update_status("Hello World!")
-'''
+
 
 def generate_planet_color():
     R = random.random()
@@ -65,19 +59,28 @@ sun = LightSource([50, 20, -15],
         'color',[1, 1, 1],
         'translate', [-5, 5, -5])
 
-planet = Sphere ( [0,0,0], 1,
-         Texture ( Pigment( 'color', generate_planet_color()),
-                    Normal ( choose_random_normal(), rMag(), 'scale', rMag(), 'turbulence', rMag())                                    
-                 ),
-          'scale',[.75,.75,.75],  
-          'rotate',[0,0,0],  
-          'translate',[0,0,0]  
-       )
+def tweet_planet(api):
+    imagePath = "./planets.png"
+    render_planet()
+    try:
+        api.update_with_media(filename=imagePath)
+    except Exception as e:
+        break
 
-scene = Scene( Camera('angle', 75,'location',  [0.0 , 0,-2.0],
-                      'look_at', [0 , 0 , 0.0]),
-               objects = [sky, sun, planet],
-               included = ["colors.inc", "textures.inc"],
-               defaults = [Finish( 'ambient', 0.1, 'diffuse', 0.9)] )
+def render_planet():
+    planet = Sphere ( [0,0,0], 1,
+            Texture ( Pigment( 'color', generate_planet_color()),
+                        Normal ( choose_random_normal(), rMag(), 'scale', rMag(), 'turbulence', rMag())                                    
+                    ),
+            'scale',[.75,.75,.75],  
+            'rotate',[0,0,0],  
+            'translate',[0,0,0]  
+        )
 
-scene.render("planet.png", includedirs=["./libraries"], width=400, height=300, antialiasing=0.001)
+    scene = Scene( Camera('angle', 75,'location',  [0.0 , 0,-2.0],
+                        'look_at', [0 , 0 , 0.0]),
+                objects = [sky, sun, planet],
+                included = ["colors.inc", "textures.inc"],
+                defaults = [Finish( 'ambient', 0.1, 'diffuse', 0.9)] )
+
+    scene.render("planet.png", includedirs=["./libraries"], width=400, height=300, antialiasing=0.001)
